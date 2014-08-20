9ea98ce5b4760f529d4cf0197af2f3d35906f539
MattWis <matthew.wismer@students.olin.edu>
2014-08-20 17:38:01 -0400

    Don't need speech.sh anymore

--- speech.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST \
---data-binary @audio/output.flac \
---header 'Content-Type: audio/x-flac; rate=16000;' \
-'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyD6Cba8xQZUMnw_FUEaeJOTOz-kBMIf6l4'
