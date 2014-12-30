fcf52a1d427d2e89031480f747374860f64c45ff
MattWis <matthew.wismer@students.olin.edu>
2014-12-30 10:21:30 -0500

    Add tests for Wit STT

--- constant_listener/pyspeechTest.py
+++ constant_listener/pyspeechTest.py
@@ -16,5 +16,17 @@ class PyspeechTest(unittest.TestCase):
     output = best_speech_result(self.p, hello_world.read(), {}, "google")
     self.assertEqual(output, "hello world")
 
+  # This will fail without a valid wit_token in profile.yml
+  def test_wit_stt(self):
+    import yaml
+    profile = yaml.load(open("profile.yml").read())
+    good_morning = open('example_wavs/good_morning.wav', 'rb')
+    output = best_speech_result(self.p, good_morning.read(), profile, "wit")
+    self.assertEqual(output, "good morning")
+
+    hello_world = open('example_wavs/hello_world.wav', 'rb')
+    output = best_speech_result(self.p, hello_world.read(), profile, "wit")
+    self.assertEqual(output, "hello world")
+
 if __name__ == "__main__":
   unittest.main()
